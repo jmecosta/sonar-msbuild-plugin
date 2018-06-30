3dffff6a8bb8b6913d4e4771bbe9a3a9bbc61b4e
Costa Jorge <jorge.costa@trimble.com>
2018-06-30 17:21:55 +0300

    release 1.5.0

--- ProjectCheckerRunner/build-log-vs2013.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-nuget\nuget restore ProjectChecker.sln
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
-msbuild ProjectChecker.sln /p:VisualStudioVersion=12.0 /p:VsVersion=12.0 /p:VsFolder=vs13  /p:Configuration=Release /v:diag > buildlog2013.txt
-
--- ProjectCheckerRunner/nuget/packanddeploy.bat
+++ ProjectCheckerRunner/nuget/packanddeploy.bat
@@ -1,5 +1,4 @@
 del *.nupkg
-nuget.exe pack MsbuildAutomationHelper431.nuspec
 nuget.exe pack MsbuildAutomationHelper440.nuspec
-nuget.exe pack MsbuildAutomationHelper441.nuspec
+nuget.exe pack MsbuildAutomationHelper443.nuspec
 nuget.exe push *.nupkg -Source https://www.nuget.org/api/v2/package
